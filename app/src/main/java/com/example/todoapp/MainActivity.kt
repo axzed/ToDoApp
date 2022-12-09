05b9c5634d2c0e3c083e9fcf1447d0957dd38bf5
axzed <waxwc1491224820@gmail.com>
2022-12-09 11:17:57 +0800

    :sparkles: feat: 完成添加页面的布局设计

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.TaskStackBuilder
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 设置标题栏
+        // 1. 获取导航控制器
+        // 2. 设置标题栏
+        // 3. 设置标题栏的返回按钮
+        setupActionBarWithNavController(findNavController(R.id.navHostFragment))
+    }
+
+    // 重写返回按钮
+    // 1. 获取导航控制器
+    // 2. 调用导航控制器的navigateUp方法
+    // 3. 返回true
+    override fun onSupportNavigateUp(): Boolean {
+        // 返回上一个页面
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -1,11 +1,18 @@
 package com.example.todoapp.fragments.list
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.todoapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListFragment : Fragment() {
 
@@ -15,7 +22,11 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list, container, false)
-
+        // 设置按钮点击事件(添加)
+        view.findViewById<FloatingActionButton>(R.id.addButton).setOnClickListener {
+            // 跳转到添加页面
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
         return view
     }
 
