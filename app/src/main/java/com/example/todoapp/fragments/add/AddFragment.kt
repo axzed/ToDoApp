97b9d8990d9cec79e241a22bdf2b7315e76c1982
axzed <waxwc1491224820@gmail.com>
2022-12-19 15:58:00 +0800

    :sparkles: feat: 完成add页面的菜单设计

--- app/src/main/java/com/example/todoapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/add/AddFragment.kt
@@ -1,10 +1,8 @@
 package com.example.todoapp.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.todoapp.R
 
 class AddFragment : Fragment() {
@@ -13,8 +11,18 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // 设置menu
+        setHasOptionsMenu(true)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    // menu
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        // 1. 加载menu
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 
 }
