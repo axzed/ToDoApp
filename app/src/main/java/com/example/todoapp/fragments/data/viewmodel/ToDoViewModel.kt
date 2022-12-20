58cb0707fb5f7126172d3c8ac14aa8f57c5376c9
axzed <waxwc1491224820@gmail.com>
2022-12-20 17:32:04 +0800

    :sparkles: add view model

---
+++ app/src/main/java/com/example/todoapp/fragments/data/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.todoapp.fragments.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.todoapp.fragments.data.ToDoDatabase
+import com.example.todoapp.fragments.data.model.ToDoData
+import com.example.todoapp.fragments.data.repository.ToDoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ToDoViewModel(application: Application) : AndroidViewModel(application) {
+
+    // 获取数据库的实例
+    private val toDoDao = ToDoDatabase.getDatabase(application).toDoDao()
+    // 获取数据库的接口
+    private val repository:  ToDoRepository
+
+    private val getAllData: LiveData<List<ToDoData>>
+
+    // 用init初始化
+    init {
+        // 用repository初始化
+        repository = ToDoRepository(toDoDao)
+        // 用getAllData初始化
+        getAllData = repository.getAllData
+    }
+
+    // 用fun修饰，返回一个Unit
+    fun insertData(toDoData: ToDoData) {
+        // 用viewModelScope.launch{}启动协程
+        viewModelScope.launch(Dispatchers.IO) {
+            // 用repository.insertData()插入数据
+            repository.insertData(toDoData)
+        }
+    }
+
+}
